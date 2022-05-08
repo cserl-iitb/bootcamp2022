393dbf956c103a912c490e347afcff06cc6122b4
Gaurang Dev <gaurangdev@Gaurangs-MacBook-Air.local>
2022-05-09 01:28:51 +0530

    updated client_sol.cpp

--- application_software/week-2/client_sol.cpp
+++ application_software/week-2/client_sol.cpp
@@ -9,40 +9,44 @@
 #include <fcntl.h>
 #include <pthread.h>
 
+struct host_port
+{
+    char* hostname;
+    int port;
+};
 void error(char *msg)
 {
     perror(msg);
     exit(0);
 }
 
-void * clientThread(int serverport)
+void *clientThread(void* server_hostport)
 {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
     char buffer[256];
 
-    portno = serverport;
+    struct host_port* hostport = (host_port*)server_hostport;
 
+    char *hostname = hostport->hostname;
+    portno = hostport->port;
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd < 0)error("ERROR opening socket");
 
-    server = gethostbyname(server);
+    server = gethostbyname(hostname);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
 
 
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) error("ERROR connecting");
 
     strcpy(buffer,"Hello");
 
@@ -70,7 +74,10 @@ int main(int argc, char *argv[])
     pthread_t tid[51];
     while(i< 50)
     {
-        if( pthread_create(&tid[i++], NULL, clientThread, atoi(argv[2])) != 0 )
+        struct host_port hostport;
+        hostport.hostname = "localhost";
+        hostport.port = atoi(argv[2]);
+        if( pthread_create(&tid[i++], NULL, clientThread, &hostport) != 0 )
             printf("Failed to create thread\n");
     }
 
