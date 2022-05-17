81da44d03f89c443aa4a65cb7457d05e1de232b5
devansh-dvj <devansh.dvj@gmail.com>
2022-05-18 02:45:49 +0530

    AST2: File header comments updated

--- application_software/week2/simple-client.c
+++ application_software/week2/simple-client.c
@@ -1,3 +1,5 @@
+/* run client using: ./client localhost <server_port> */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- application_software/week2/simple-server.c
+++ application_software/week2/simple-server.c
@@ -1,5 +1,4 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
+/* run using ./server <port> */
 
 #include <stdio.h>
 #include <stdlib.h>
