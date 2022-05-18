528347e20335b9b7f3c7a77d8b58d6053fc96d09
KALYAN RAM CHIMMILI <101321732+kalyanramchimmilli@users.noreply.github.com>
2022-05-18 20:15:14 +0530

    Update fibbonaci.cpp
    making i dependent on int n and avoiding printing of first digit 2 times by removing additional command

--- week1_warmup/Debugging_with_GDB/fibbonaci.cpp
+++ week1_warmup/Debugging_with_GDB/fibbonaci.cpp
@@ -3,15 +3,14 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-  int n = 10;
-
+ int n=10;
   int second_last = 1;
   int last = 1;
 
-  cout << second_last << endl << last << endl;
+  cout << second_last << endl ;
 
-  for(int i=1; i<=10; i++) {
-    int next = second_last + last;
+  for(int i=1; i<=n; i++) {
+    int next = second_last + last ;
     cout << next << endl;
     last = next;
     second_last = last;
