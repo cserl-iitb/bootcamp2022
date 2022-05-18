c9513513da79e66013950516b0b9684186700855
KALYAN RAM CHIMMILI <101321732+kalyanramchimmilli@users.noreply.github.com>
2022-05-18 20:01:20 +0530

    int *q = &b;
    p = &b;cout << *p << endl; is eliminated
    int *q = &b;// b is taken instead of null

--- week1_warmup/Debugging_with_GDB/pointers.cpp
+++ week1_warmup/Debugging_with_GDB/pointers.cpp
@@ -5,15 +5,13 @@ int main(int argc, char* argv[]) {
 
   int a = 45; int b = 22;
   int *p = &a;
-  int *q = NULL;
+  int *q = &b;// b is taken instead of null
 
   cout << *p << endl;
 
   p = q;
   cout << *p << endl;
-
-  p = &b;
-  cout << *p << endl;
-
+//p = &b;cout << *p << endl; is eliminated
+  
   return 0;
 }
