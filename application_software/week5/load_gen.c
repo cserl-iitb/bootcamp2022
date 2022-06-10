6ebfcbf0c1e416671f1dc25defef8a4d0f01c4b3
devansh-dvj <devansh.dvj@gmail.com>
2022-06-10 18:40:51 +0530

    AST5: Added template for load generator

--- application_software/week5/load_gen.c
+++ application_software/week5/load_gen.c
@@ -1,5 +1,5 @@
-/* run client using: ./client localhost <server port> <concurrency> <think time>
- * <test duration> */
+/* run using: ./load_gen localhost <server port> <number of concurrent users>
+   <think time (in s)> <test duration (in s)> */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,18 +8,89 @@
 
 #include <netdb.h>
 
+#include <pthread.h>
+#include <sys/time.h>
+
+int time_up;
+FILE *log_file;
+
+// user info struct
+struct user_info {
+  // user id
+  int id;
+
+  // socket info
+  int portno;
+  char *hostname;
+  float think_time;
+
+  // user metrics
+  int total_count;
+  float total_rtt;
+};
+
+// error handling function
 void error(char *msg) {
   perror(msg);
   exit(0);
 }
 
-int main(int argc, char *argv[]) {
-  int sockfd, portno, n;
+// time diff in seconds
+float time_diff(struct timeval *t2, struct timeval *t1) {
+  return (t2->tv_sec - t1->tv_sec) + (t2->tv_usec - t1->tv_usec) / 1e6;
+}
+
+// user thread function
+void *user_function(void *arg) {
+  /* get user info */
+  struct user_info *info = (struct user_info *)arg;
+
+  int sockfd, n;
+  char buffer[256];
+  struct timeval start, end;
 
   struct sockaddr_in serv_addr;
   struct hostent *server;
 
-  char buffer[256];
+  while (1) {
+    /* start timer */
+    gettimeofday(&start, NULL);
+
+    /* TODO: create socket */
+
+    /* TODO: set server attrs */
+
+    /* TODO: connect to server */
+
+    /* TODO: send message to server */
+
+    /* TODO: read reply from server */
+
+    /* TODO: close socket */
+
+    /* end timer */
+    gettimeofday(&end, NULL);
+
+    /* if time up, break */
+    if (time_up)
+      break;
+
+    /* TODO: update user metrics */
+
+    /* TODO: sleep for think time */
+  }
+
+  /* exit thread */
+  fprintf(log_file, "User #%d finished\n", info->id);
+  fflush(log_file);
+  pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+  int user_count, portno, test_duration;
+  float think_time;
+  char *hostname;
+
   if (argc != 6) {
     fprintf(stderr,
             "Usage: %s <hostname> <server port> <number of concurrent users> "
@@ -28,52 +99,50 @@ int main(int argc, char *argv[]) {
     exit(0);
   }
 
-  printf("%s %s %s %s %s\n", argv[1], argv[2], argv[3], argv[4], argv[5]);
-
-  /* create socket, get sockfd handle */
-
+  hostname = argv[1];
   portno = atoi(argv[2]);
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0)
-    error("ERROR opening socket");
+  user_count = atoi(argv[3]);
+  think_time = atof(argv[4]);
+  test_duration = atoi(argv[5]);
 
-  /* fill in server address in sockaddr_in data structure */
+  printf("Hostname: %s\n", hostname);
+  printf("Port: %d\n", portno);
+  printf("User Count: %d\n", user_count);
+  printf("Think Time: %f s\n", think_time);
+  printf("Test Duration: %d s\n", test_duration);
 
-  server = gethostbyname(argv[1]);
-  if (server == NULL) {
-    fprintf(stderr, "ERROR, no such host\n");
-    exit(0);
+  /* open log file */
+  log_file = fopen("load_gen.log", "w");
+
+  pthread_t threads[user_count];
+  struct user_info info[user_count];
+  struct timeval start, end;
+
+  /* start timer */
+  gettimeofday(&start, NULL);
+  time_up = 0;
+  for (int i = 0; i < user_count; ++i) {
+    /* TODO: initialize user info */
+
+    /* TODO: create user thread */
+
+    fprintf(log_file, "Created thread %d\n", i);
   }
-  bzero((char *)&serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr,
-        server->h_length);
-  serv_addr.sin_port = htons(portno);
 
-  /* connect to server */
+  /* TODO: wait for test duration */
 
-  if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    error("ERROR connecting");
+  fprintf(log_file, "Woke up\n");
 
-  /* ask user for input */
+  /* end timer */
+  time_up = 1;
+  gettimeofday(&end, NULL);
 
-  printf("Please enter the message: ");
-  bzero(buffer, 256);
-  fgets(buffer, 255, stdin);
+  /* TODO: wait for all threads to finish */
 
-  /* send user message to server */
+  /* TODO: print results */
 
-  n = write(sockfd, buffer, strlen(buffer));
-  if (n < 0)
-    error("ERROR writing to socket");
-  bzero(buffer, 256);
-
-  /* read reply from server */
-
-  n = read(sockfd, buffer, 255);
-  if (n < 0)
-    error("ERROR reading from socket");
-  printf("Server response: %s\n", buffer);
+  /* close log file */
+  fclose(log_file);
 
   return 0;
 }
